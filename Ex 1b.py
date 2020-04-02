b6366f875efcfe8220e7fe22655bed23ee08667d
jeeva3710 <63037469+jeeva3710@users.noreply.github.com>
2020-04-02 12:40:33 +0530

    Update Ex 1b.py

--- Ex 1b.py
+++ Ex 1b.py
@@ -2,7 +2,7 @@ a=input("Enter 1st number")
 b=input("Enter 2nd number")
 c=input("Enter 2nd number")
 if a>b and a>c:
-    print("Greater Nmber is",a)
+    print("Greater Number is",a)
 elif b>c:
     print("Greater Number is",b)
 else:
