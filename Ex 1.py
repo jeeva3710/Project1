296a9a71ab1a721d0b875b4e237ac43284e707dd
jeeva3710 <63037469+jeeva3710@users.noreply.github.com>
2020-04-02 12:03:49 +0530

    Create Ex 1.py

---
+++ Ex 1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition",c)
+print("Subtraction",d)
+print("Multiplication",e)
+print("Division",f)
