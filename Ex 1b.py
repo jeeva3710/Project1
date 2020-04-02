4432394db76da98889f8653ef707ba36336713a4
jeeva3710 <63037469+jeeva3710@users.noreply.github.com>
2020-04-02 12:32:11 +0530

    Two files added

---
+++ Ex 1b.py
@@ -0,0 +1,9 @@
+a=input("Enter 1st number")
+b=input("Enter 2nd number")
+c=input("Enter 2nd number")
+if a>b and a>c:
+    print("Greater Nmber is",a)
+elif b>c:
+    print("Greater Number is",b)
+else:
+    print("Greater Number is",c)
