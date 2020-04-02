262ed6965da0227451f1f01eb9cf52968ff1051d
jeeva3710 <63037469+jeeva3710@users.noreply.github.com>
2020-04-02 12:27:40 +0530

    Update Ex 1.py

--- Ex 1.py
+++ Ex 1.py
@@ -4,7 +4,11 @@ c=a+b
 d=a-b
 e=a*b
 f=a/b
+g=a//b
+h=a**b
 print("Addition",c)
 print("Subtraction",d)
 print("Multiplication",e)
 print("Division",f)
+print("Floor Division",g)
+print("Exponentiation",h)
